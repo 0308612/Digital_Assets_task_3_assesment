35b14ef9ef50bd8150b5135945343f4bc4a0c8d2
sebastianSchwenke <96216134+0308612@users.noreply.github.com>
2025-05-20 11:20:07 +1000

    added start page
    problems - sizing of form was bigger than rest changing form size didn't work added max size as size i wanted worked

---
+++ startPage.cs
@@ -0,0 +1,81 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using System.IO;
+
+namespace task_3_build_assesment
+{
+    public partial class startPage : Form
+    {
+        public startPage()
+        {
+            InitializeComponent();
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            welcomeVaribles f2 = new welcomeVaribles();
+            f2.Owner = this;
+            f2.StartPosition = FormStartPosition.Manual;
+            f2.Location = this.Location;
+            f2.Show();
+            this.Hide();
+        }
+
+        private void button2_Click(object sender, EventArgs e)
+        {
+            loops f3 = new loops();
+            f3.Owner = this;
+            f3.StartPosition = FormStartPosition.Manual;
+            f3.Location = this.Location;
+            f3.Show();
+            this.Hide();
+        }
+
+        private void button3_Click(object sender, EventArgs e)
+        {
+            random f4 = new random();
+            f4.Owner = this;
+            f4.StartPosition = FormStartPosition.Manual;
+            f4.Location = this.Location;
+            f4.Show();
+            this.Hide();
+        }
+
+        private void button4_Click(object sender, EventArgs e)
+        {
+            arrays f5 = new arrays();
+            f5.Owner = this;
+            f5.StartPosition = FormStartPosition.Manual;
+            f5.Location = this.Location;
+            f5.Show();
+            this.Hide();
+        }
+
+        private void button5_Click(object sender, EventArgs e)
+        {
+            functions f6 = new functions();
+            f6.Owner = this;
+            f6.StartPosition = FormStartPosition.Manual;
+            f6.Location = this.Location;
+            f6.Show();
+            this.Hide();
+        }
+
+        private void button6_Click(object sender, EventArgs e)
+        {
+            code f7 = new code();
+            f7.Owner = this;
+            f7.StartPosition = FormStartPosition.Manual;
+            f7.Location = this.Location;
+            f7.Show();
+            this.Hide();
+        }
+    }
+}
