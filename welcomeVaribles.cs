dd3b8bcbed461af5d82c1502e3958055174542d5
sebastianSchwenke <96216134+0308612@users.noreply.github.com>
2025-05-20 11:22:35 +1000

    added first tutorial
    added form and back button

---
+++ welcomeVaribles.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace task_3_build_assesment
+{
+    public partial class welcomeVaribles : Form
+    {
+        public welcomeVaribles()
+        {
+            InitializeComponent();
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            this.Owner.Show();
+            this.Close();
+        }
+    }
+}
